08f58ef30b72b440fc73ad9a18bcde0de40df32c
NilCerquera <nifecerquera@gmail.com>
2024-05-06 18:34:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Proyecto Urban Grocers 
---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
+CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
+KITS_PATH = # Almacena la ruta para crear un kit en esta variable
